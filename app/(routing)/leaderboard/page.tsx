168cff3102b8afad6b1716133c44ecd080e4addc
Giselle728 <giselledmello728@student.sfit.ac.in>
2026-04-11 12:10:29 +0530

    create main routing structure for auth, challenge, leaderboard, profile, and pvp

---
+++ app/(routing)/leaderboard/page.tsx
@@ -0,0 +1,10 @@
+import Leaderboard from "../../components/leaderboard/page"
+
+export default function Page() {
+  return (
+    <main className="relative z-10 p-8 min-h-screen">
+      <Leaderboard />
+    </main>
+  );
+}
+
---
+++ app/(routing)/profile/page.tsx
@@ -0,0 +1,9 @@
+import Profile from "../../components/profile/page"
+
+export default function Page() {
+  return (
+    <main className="relative z-10 p-8 min-h-screen">
+      < Profile/>
+    </main>
+  )
+}
